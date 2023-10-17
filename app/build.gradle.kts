9c69f8cbbd353e312e77f9a604f49886874a7417
anxdre <anxdre@protonmail.com>
2023-10-17 10:53:49 +0700

    Modify : Updating dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
-    id("kotlin-parcelize")
 }
 
 android {
@@ -44,7 +43,7 @@ dependencies {
     //core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     //view
@@ -52,6 +51,7 @@ dependencies {
     implementation ("com.afollestad.material-dialogs:input:3.3.0")
     implementation ("com.airbnb.android:lottie:6.1.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
     //roomDB
     val roomVersion = "2.5.2"
@@ -59,12 +59,15 @@ dependencies {
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
 
     //navigation
-    val navVersion = "2.7.3"
+    val navVersion = "2.7.4"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     //serialization
     implementation ("com.google.code.gson:gson:2.10.1")
+
+    //networking
+    implementation ("com.android.volley:volley:1.2.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
